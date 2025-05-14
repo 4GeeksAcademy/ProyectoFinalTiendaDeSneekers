cf0a6f77d3915303a2cba43120c5862b94fb6711
DaniVera-tech <daniss.vera@gmail.com>
2025-05-14 18:23:23 +0200

    Revert "Bdd"

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,51 +1,41 @@
-import React from "react";
-
-import { FaUser, FaHeart, FaShoppingBag } from "react-icons/fa";
-
-const NavbarMenu = () => {
-  return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Hombre</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Mujer</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Niño</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Ofertas
-              </a>
-              <ul className="dropdown-menu">
-                <li><a className="dropdown-item" href="#">Todas las ofertas</a></li>
-                <li><hr className="dropdown-divider" /></li>
-                <li><a className="dropdown-item" href="#">Hombre</a></li>
-                <li><a className="dropdown-item" href="#">Mujer</a></li>
-                <li><a className="dropdown-item" href="#">Niños</a></li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-            </li>
+<nav classname="navbar navbar-expand-lg bg-body-tertiary">
+  <div classname="container-fluid">
+    <a classname="navbar-brand" href="#">Navbar</a>
+    <button classname="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span classname="navbar-toggler-icon"></span>
+    </button>
+    <div classname="collapse navbar-collapse" id="navbarSupportedContent">
+      <Navbar.Brand href="/"> <img src="https://w7.pngwing.com/pngs/672/449/png-transparent-sneakers-shoe-graphy-others-white-logo-monochrome.png" alt="Logo" width="120" /> </Navbar.Brand>
+      <ul classname="navbar-nav me-auto mb-2 mb-lg-0">
+        <li classname="nav-item">
+          <a classname="nav-link active" aria-current="page" href="#">Hombre</a>
+        </li>
+        <li classname="nav-item">
+          <a classname="nav-link" href="#">Mujer</a>
+        </li> 
+        <li classname="nav-item">
+          <a classname="nav-link" href="#">Niño</a>
+        </li>
+        <li classname="nav-item dropdown">
+          <a classname="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Ofertas
+          </a>
+          <ul classname="dropdown-menu">
+            <li><a classname="dropdown-item" href="#">Todas las ofertas</a></li>
+            <li><hr classname="dropdown-divider"/></li>
+            <li><a classname="dropdown-item" href="#">Hombre</a></li>
+            <li><a classname="dropdown-item" href="#">Mujer</a></li>
+            <li><a classname="dropdown-item" href="#">Niños</a></li>
           </ul>
-          <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
-          </form>
-        </div>
-      </div>
-    </nav>
-  );
-};
-
-export default NavbarMenu;
-
+        </li>
+        <li classname="nav-item">
+          <a classname="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form classname="d-flex" role="search">
+        <input classname="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button classname="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
