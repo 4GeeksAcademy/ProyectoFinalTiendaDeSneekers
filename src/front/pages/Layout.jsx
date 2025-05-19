cf7503fdb9a56e85dac3d3defc75ea4297f73600
raulbordon <raulbordon2002@hotmail.es>
2025-05-19 09:06:30 +0000

    primer jumbotron

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -3,14 +3,15 @@ import ScrollToTop from "../components/ScrollToTop"
 import  Footer  from "../components/Footer"
 import NavbarMenu from "../components/Navbar"
 import Body from "../components/Body";
+import Jumbotron from "../components/Jumbotron"
 
 // Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
     return (
         <ScrollToTop>
             <NavbarMenu />
+            <Jumbotron/>
             <Body/>
-            <Outlet />
             <Footer />
         </ScrollToTop>
     )
